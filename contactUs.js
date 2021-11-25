db06f5a4c17308ba26d74606dd65b22b09aaa678
abdelrahmanelsese1 <abdelrahmanmohamedkhaleel@gmail.com>
2021-11-25 23:12:30 +0200

    contact us page

---
+++ contactUs.js
@@ -0,0 +1,15 @@
+function formValid(e){
+    var email=document.getElementById("mail")
+    var name=document.getElementById("name")
+    if(email.value==""){
+        e.preventDefault
+    }
+    else if(name.value.length<3){
+        e.perventDefault
+    }
+    else{
+        e.target.submit
+        
+    }
+    
+}  
