508543514f7531fd866629169b3e07adf2724fa3
ALaa Farouk <alaafar19@gmail.com>
2021-11-26 17:09:24 +0200

    validate sign up :+1:

--- media/signup.js
+++ media/signup.js
@@ -3,7 +3,11 @@ var username = document.getElementById("username") ;
 var useremail = document.getElementById("email") ; 
 var userpass = document.getElementById("password") ; 
 var confirmpass = document.getElementById("Confirmpass") ;
+console.log(username) ;
+console.log(useremail);
+debugger ; 
 function CheckName() {
+    debugger ; 
     if (username.length < 3 || username == "") {
         username.setCustomValidity("Please Follow the requiements");
     }
@@ -13,6 +17,7 @@ function CheckName() {
     }
 }
 function validatePassword(){
+    debugger ; 
     if(userpass.value != confirmpass.value) {
         confirmpass.setCustomValidity("Passwords Don't Match");
     } else {
