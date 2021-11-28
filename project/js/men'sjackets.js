23ae14b0543b96f0fad9cf31fecb70c7488f9f3f
abdelrahmanelsese1 <abdelrahmanmohamedkhaleel@gmail.com>
2021-11-28 14:54:53 +0200

    edits

--- project/js/men'sjackets.js
+++ project/js/men'sjackets.js
@@ -1,4 +1,12 @@
 
-function myfunction(){
+function product1Desc(){
+    location.href="aboutUs.html"
+}
+
+function product2Desc(){
+    location.href="aboutUs.html"
+}
+
+function product3Desc(){
     location.href="aboutUs.html"
 }
